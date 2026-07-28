f64b50071561e2eeea1c96125754541feea8f786
dharunramp <dharunramana2006@gmail.com>
2026-07-28 20:23:24 +0530

    operators

---
+++ operators.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int m1, m2, m3, m4, m5;
+
+    printf("Enter marks of 5 subjects: ");
+    scanf("%d %d %d %d %d", &m1, &m2, &m3, &m4, &m5);
+
+    if (m1 >= 40 && m2 >= 40 && m3 >= 40 && m4 >= 40 && m5 >= 40)
+    {
+        printf("Pass");
+    }
+    else
+    {
+        printf("Fail");
+    }
+
+    return 0;
+}
