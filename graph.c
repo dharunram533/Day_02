dde21035820d7a341999a1524cb601b89ca5a6c6
Dharun Ram <dharunramana2006@gmail.com>
2026-07-28 22:38:46 +0530

    Update graph.c
    Added a program to determine the quadrant of a point based on its coordinates.

--- graph.c
+++ graph.c
@@ -1,3 +1,7 @@
+QUESTION:
+Write a program that accepts the x and y coordinates of a point and determines in which quadrant the point lies.
+
+CODE:
 #include <stdio.h>
 
 int main()
@@ -38,3 +42,20 @@ int main()
 
     return 0;
 }
+
+
+OUTPUT:
+Enter x and y coordinates: 5 8
+The point lies in First Quadrant
+
+
+Enter x and y coordinates: -4 6
+The point lies in Second Quadrant
+
+
+Enter x and y coordinates: -3 -7
+The point lies in Third Quadrant
+
+
+Enter x and y coordinates: 4 -5
+The point lies in Fourth Quadrant
