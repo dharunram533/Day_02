9b1d667503285ea14b6afaa340b197b360ed8afc
dharunramp <dharunramana2006@gmail.com>
2026-07-28 20:36:06 +0530

    if-else

---
+++ divisible.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+
+    if (n % 5 == 0 && n % 11 == 0)
+    {
+        printf("%d is divisible by both 5 and 11.\n", n);
+    }
+    else
+    {
+        printf("%d is not divisible by both 5 and 11.\n", n);
+    }
+
+    return 0;
+}
