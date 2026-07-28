d0b79c2c5de20948ace60871b0ae6b00e55e3b9c
Dharun Ram <dharunramana2006@gmail.com>
2026-07-28 22:29:59 +0530

    Update determine_letter.c
    Added functionality to determine character type: uppercase, lowercase, digit, or special symbol.

--- determine_letter.c
+++ determine_letter.c
@@ -1,3 +1,7 @@
+QUESTION:
+Write a program to accept a single character from the keyboard and determine whether it is an uppercase letter, lowercase letter, digit, or special symbol.
+
+CODE:
 # include <stdio.h>
 int main()
 {
@@ -22,3 +26,20 @@ int main()
  }
 return 0;
 }
+
+OUTPUT:
+Enter a character: A
+It is an Uppercase letter
+
+
+Enter a character: a
+It is a Lowercase letter
+
+
+Enter a character: 7
+It is a Digit
+
+
+Enter a character: @
+It is a Special Symbol
+
